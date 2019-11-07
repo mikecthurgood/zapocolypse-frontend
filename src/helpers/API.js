027e9d7bae0d89b1107fbc8641bd5f3012a4bdc2
github username <github email address>
2019-11-07 15:57:51 +0000

    API calls, signin, validates

---
+++ src/helpers/API.js
@@ -0,0 +1,30 @@
+const baseUrl = 'http://localhost:3000/'
+const signInUrl = baseUrl + '/signin'
+const validateUrl = baseUrl + '/validate'
+
+class API {
+    static signIn = (username, password) => this.post(signInUrl, {username, password})
+    
+    static validates = () => this.get(validateUrl)
+
+    static get = (url) => 
+        fetch(url, {
+            headers: {
+                Authorization: localStorage.getItem('token')
+            }
+        }).then(resp => resp.json())
+    
+
+    static post = (url, data) =>
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(resp => resp.json())
+    
+    
+}
+
+export default API
