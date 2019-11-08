5ff0871594ab63ef57bd3cdff635b4a1497d51fd
github username <github email address>
2019-11-08 15:53:43 +0000

    added radar chart on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import captions from '../helpers/chartCaptions';
+import RadarChart from 'react-svg-radar-chart';
+import './data.css';
 
 class Home extends React.Component {
 
@@ -8,9 +11,42 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div className='chart'>
+                <RadarChart
+                    captions={captions}
+                    data={data}
+                    size={450}
+                />
+            </div>
         )
     }
 }
+
+const data = [
+    {
+      data: {
+        survival: 0.7,
+        combat: .8,
+        fitness: 0.9,
+        intelligence: 0.67,
+        engineering: 0.8,
+        medical: 0.1
+      },
+      meta: { color: 'green' }
+    },
+    {
+      data: {
+        survival: 0.6,
+        combat: .85,
+        fitness: 0.5,
+        intelligence: 0.6,
+        engineering: 0.7,
+        medical: 0.3
+      },
+      meta: { color: 'red' }
+    }
+  ];
+
+
 
 export default Home 
