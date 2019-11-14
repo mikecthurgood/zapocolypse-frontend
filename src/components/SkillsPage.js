16a3e7b6376181302b25a78e57fc78e4d75cd1c0
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-14 15:10:17 +0000

    styling changes on home

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -4,7 +4,7 @@ import SkillCard from './SkillCard'
 import ActivityCard from './ActivityCard'
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import {isEmpty} from '../helpers/utils'
+import { isEmpty } from '../helpers/utils'
 
 class SkillsPage extends React.Component {
 
@@ -14,29 +14,31 @@ class SkillsPage extends React.Component {
         zaps: {},
         singleSkill: true
     }
-    
+
     componentDidMount() {
         API.getSkill(this.props.match.params.id)
-        .then(data => { this.setState(
-            { 
-                skill: data.skill, 
-                activities: data.activities, 
-                zaps: data.skillZaps
-            })})
+            .then(data => {
+                this.setState(
+                    {
+                        skill: data.skill,
+                        activities: data.activities,
+                        zaps: data.skillZaps
+                    })
+            })
     }
 
     skillLevel = (val) => {
-        if (val>=0 && val < 5) {
+        if (val >= 0 && val < 5) {
             return 'Ignorant'
-        }else if (val>=5 && val < 20){
+        } else if (val >= 5 && val < 20) {
             return 'Noob'
-        }else if (val>=20 && val < 100){
+        } else if (val >= 20 && val < 100) {
             return 'Disciple'
-        }else if (val>=100 && val < 500){
+        } else if (val >= 100 && val < 500) {
             return 'Adept'
-        }else if (val>=500 && val < 1000){
+        } else if (val >= 500 && val < 1000) {
             return 'Expert'
-        }else if (val>=1000){
+        } else if (val >= 1000) {
             return 'Master'
         }
     }
@@ -45,17 +47,13 @@ class SkillsPage extends React.Component {
         if (!isEmpty(zaps)) {
             return (
                 <CircularProgressbarWithChildren value={zaps[skill.name]}>
-                    <div className='inside-progress-bar'>
-                        <div className='prog-bar-elements'><strong>{zaps[skill.name]}⚡️</strong> </div><br/>
-                        <div className='prog-bar-elements'>{this.skillLevel(zaps[skill.name])}</div>
-                    </div>
                 </CircularProgressbarWithChildren>
             )
         }
         return (
             <CircularProgressbarWithChildren value={0}>
                 <div className='inside-progress-bar'>
-                    <div className='prog-bar-elements'><strong>{0}⚡️</strong> </div><br/>
+                    <div className='prog-bar-elements'><strong>{0}⚡️</strong> </div><br />
                     <div className='prog-bar-elements'>{this.skillLevel(0)}</div>
                 </div>
             </CircularProgressbarWithChildren>
@@ -63,7 +61,7 @@ class SkillsPage extends React.Component {
     }
 
     render() {
-        const {skill, activities, zaps} = this.state
+        const { skill, activities, zaps } = this.state
         return (
             <div className='skill-page'>
                 <div skill-page-header>
@@ -71,6 +69,10 @@ class SkillsPage extends React.Component {
                     <div className='skill-header'>
                         <div>{skill.name && < SkillCard key={skill.id} {...skill} singleSkill={this.state.singleSkill} />}</div>
                         <div className='progress-chart'>{this.showProgressBar(zaps, skill)}</div>
+                        <div>
+                            <div className='skillpage-zaps'><strong>{zaps[skill.name]}⚡️</strong>
+                                {this.skillLevel(zaps[skill.name])}</div>
+                        </div>
                     </div>
                 </div>
 
